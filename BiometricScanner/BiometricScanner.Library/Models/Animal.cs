2c0ab315258f534a012f71e06a12ae3bfa66773a
nicolasgrillo <nicolasgrillo@live.com.ar>
2019-01-24 23:51:46 +0100

    Adding basic structures to Library

---
+++ BiometricScanner/BiometricScanner.Library/Models/Animal.cs
@@ -0,0 +1,26 @@
+﻿using BiometricScanner.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiometricScanner.Library.Models
+{
+    public abstract class Animal : IAnimal, IWalker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string MakeNoise()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Walk()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
